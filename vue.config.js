8d64c3f811ab167811ee1fea6e10b501b2cb88cf
Danil <d.pidhainyi@amifactory.team>
2024-11-14 15:11:22 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
-const { fileURLToPath, URL } = require("node:url");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' ? '/pilot/' : '/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
-  },
 });
